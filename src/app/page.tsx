fce0af366e388a455a74e5b837e34cc5a82f6fc9
eslamsemedo <eslamsemedo@gmail.com>
2025-04-09 05:16:53 +0200

    fix 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import About from '../../components/About';
 import Services from '../../components/Services';
 import Contact from '../../components/Contact';
 import Footer from '../../components/Footer';
-import Eye from '../../components/eye';
+// import Eye from '../../components/eye';
 
 export default function Home() {
   return (
